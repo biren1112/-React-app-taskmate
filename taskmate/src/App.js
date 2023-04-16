696e3bce179c1c459935d468e3878e6e7d3665d7
biren1112 <birenpatel0210@gmail.com>
2023-04-16 11:23:24 +0530

    fewrfer

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -7,7 +7,7 @@ function App() {
   function handleAdd() {
     setCount(count => count + 1);
     // setCount(count => count + 1);
-    // setCount(count => count + 1);
+    setCount(count => count + 1);
 
   }
 
